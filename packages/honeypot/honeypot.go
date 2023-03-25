7120168cfeff2e88657bc1562758bf0f42dfcfd2
timbastin <bastin.tim@gmail.com>
2023-03-25 15:11:49 +0100

    Added http port honeypot

--- packages/honeypot/honeypot.go
+++ packages/honeypot/honeypot.go
@@ -6,10 +6,6 @@ type Honeypot interface {
 	// Start starts the honeypot
 	// should not block
 	Start() error
-	// Stop stops the honeypot
-	Stop() error
-	// GetPort returns the port the honeypot is listening on
-	GetPort() int
 	// GetSETChannel returns the channel the honeypot is posting SET events to.
 	GetSETChannel() <-chan set.Token
 }
