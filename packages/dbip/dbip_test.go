e64e355f518f0f6a65a93488ca6a111bc74ee2f1
timbastin <bastin.tim@gmail.com>
2023-03-27 10:45:20 +0200

    Fixed dbip

--- packages/dbip/dbip_test.go
+++ packages/dbip/dbip_test.go
@@ -7,9 +7,30 @@ import (
 	"gitlab.com/neuland-homeland/honeypot/packages/dbip"
 )
 
-func TestLookup(t *testing.T) {
+func TestLookup1(t *testing.T) {
 	dbIp := dbip.NewIpToCountry("../../dbip-country.csv")
+
 	ip := net.ParseIP("1.1.0.0")
+	country := dbIp.Lookup(ip)
+	if country != "CN" {
+		t.Errorf("Expected CN, got %s", country)
+	}
+}
+
+func TestCompare(t *testing.T) {
+	// 60.213.192.0,60.223.255.255,CN
+	r := dbip.IPRange{Start: dbip.IP2int(net.ParseIP("60.213.192.0")), End: dbip.IP2int(net.ParseIP("60.223.255.255")), Country: "CN"}
+
+	actual := r.Compare(dbip.IPRange{Start: dbip.IP2int(net.ParseIP("60.221.224.113")), End: dbip.IP2int(net.ParseIP("60.221.224.113"))})
+
+	if actual != 0 {
+		t.Errorf("Expected 0, got %d", actual)
+	}
+}
+func TestLookup2(t *testing.T) {
+	dbIp := dbip.NewIpToCountry("../../dbip-country.csv")
+
+	ip := net.ParseIP("60.221.224.113")
 	country := dbIp.Lookup(ip)
 	if country != "CN" {
 		t.Errorf("Expected CN, got %s", country)
