048ffd208e1d24f8a255ceefd3558fe8a88592b0
Byungji Kang <justkhbby@naver.com>
2024-04-05 13:47:34 +0200

    store ssh username/pw in DB

---
+++ packages/honeypot/ssh.go
@@ -0,0 +1,169 @@
+package honeypot
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"database/sql"
+	"encoding/pem"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
+	"gitlab.com/neuland-homeland/honeypot/packages/set"
+	"gitlab.com/neuland-homeland/honeypot/packages/utils"
+	"golang.org/x/crypto/ssh"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "123"
+	dbname   = "honeypot"
+)
+
+type sshHoneypot struct {
+	port int
+	// setChan is the channel the honeypot is posting SET events to.
+	setChan chan set.Token
+}
+
+type SSHConfig struct {
+	Port int
+}
+
+func (s *sshHoneypot) Start() error {
+	config := &ssh.ServerConfig{
+		//Define a function to run when a client attempts a password login
+		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
+			// always return an error - just log the username and password
+			sub, _ := utils.NetAddrToIpStr(c.RemoteAddr())
+			s.setChan <- set.Token{
+				SUB: sub,
+				ISS: "gitlab.com/neuland-homeland/honeypot/packages/honeypot/ssh",
+				IAT: time.Now().Unix(),
+				JTI: uuid.New().String(),
+				TOE: time.Now().Unix(),
+				Events: map[string]map[string]interface{}{
+					SSHEventID: {
+						"username": c.User(),
+						"password": string(pass),
+					},
+				},
+			}
+			err := dbstore(c.User(), string(pass))
+			if err != nil {
+				log.Println(err)
+
+			}
+
+			log.Println("Login attempt:", c.User(), string(pass), c.RemoteAddr())
+			return nil, fmt.Errorf("password rejected for %q", c.User())
+		},
+	}
+
+	privateBytes := encodePrivateKeyToPEM(generatePrivateKey())
+	private, err := ssh.ParsePrivateKey(privateBytes)
+
+	if err != nil {
+		log.Fatal("Failed to parse private key")
+	}
+
+	config.AddHostKey(private)
+
+	listener, err := net.Listen("tcp", "0.0.0.0:"+fmt.Sprintf("%d", s.port))
+	if err != nil {
+		log.Fatalf("Failed to listen on %d (%s)", s.port, err)
+	}
+
+	// Accept all connections
+	log.Printf("SSH Honeypot started on %d...", s.port)
+	go func() {
+		for {
+			tcpConn, err := listener.Accept()
+			if err != nil {
+				log.Printf("Failed to accept incoming connection (%s)", err)
+				continue
+			}
+
+			go handeConn(tcpConn, config)
+		}
+	}()
+	return nil
+}
+
+func handeConn(tcpConn net.Conn, config *ssh.ServerConfig) {
+	// just perform the handshake
+	_, _, _, err := ssh.NewServerConn(tcpConn, config)
+	if err != nil {
+		return
+	}
+}
+
+func (s *sshHoneypot) Stop() error {
+	return nil
+}
+
+func (s *sshHoneypot) GetPort() int {
+	return s.port
+}
+
+func (s *sshHoneypot) GetSETChannel() <-chan set.Token {
+	return s.setChan
+}
+
+func generatePrivateKey() *rsa.PrivateKey {
+	// generate a private key
+	private, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err)
+	}
+	return private
+}
+
+func encodePrivateKeyToPEM(privateKey *rsa.PrivateKey) []byte {
+	// Get ASN.1 DER format
+	privDER := x509.MarshalPKCS1PrivateKey(privateKey)
+
+	// pem.Block
+	privBlock := pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   privDER,
+	}
+
+	// Private key in PEM format
+	privatePEM := pem.EncodeToMemory(&privBlock)
+
+	return privatePEM
+}
+
+func NewSSH(config SSHConfig) Honeypot {
+	return &sshHoneypot{
+		port:    config.Port,
+		setChan: make(chan set.Token),
+	}
+}
+
+func dbstore(id string, pw string) error {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`
+		INSERT INTO ssh (username, password)
+		VALUES ($1, $2);	
+	`, id, pw)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
