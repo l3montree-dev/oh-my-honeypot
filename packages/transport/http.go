6ace437d07d0cc66d9f62488c778f9b29131b228
Byungji Kang <justkhbby@naver.com>
2024-05-11 16:36:34 +0200

    Improved code structure

--- packages/transport/http.go
+++ packages/transport/http.go
@@ -29,9 +29,6 @@ type httpTransport struct {
 	port   int
 	getter getter
 }
-type myServeMux struct {
-	mux http.ServeMux
-}
 
 func marshalMsgs(r *http.Request, msgs []set.Token) ([]byte, error) {
 	if r.URL.Query().Get("format") == "csv" || r.Header.Get("Accept") == "text/csv" {
@@ -51,146 +48,13 @@ func marshalMsgs(r *http.Request, msgs []set.Token) ([]byte, error) {
 func (h *httpTransport) Listen() {
 	// create a new http server
 	mux := http.NewServeMux()
-	mux.Handle("/attacks-in-24hours", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			// check if the request would like a json or a csv response - default is json
-			// but csv is much smaller
-			msgs := h.getter.GetAttacksIn24Hours()
-			arr, err := marshalMsgs(r, msgs)
-
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-			w.WriteHeader(http.StatusOK)
-			w.Write(arr) // nolint
-			return
-		}
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}))
-	mux.Handle("/stats/ip-address", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			// check if the request would like a json or a csv response - default is json
-			// but csv is much smaller
-			msgs := h.getter.GetStatsIP()
-			arr, err := marshalMsgs(r, msgs)
-
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-			w.WriteHeader(http.StatusOK)
-			w.Write(arr) // nolint
-			return
-		}
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}))
-	mux.Handle("/stats/country", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			// check if the request would like a json or a csv response - default is json
-			// but csv is much smaller
-			msgs := h.getter.GetStatsCountry()
-			arr, err := marshalMsgs(r, msgs)
-
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-			w.WriteHeader(http.StatusOK)
-			w.Write(arr) // nolint
-			return
-		}
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}))
-	mux.Handle("/stats/port", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			// check if the request would like a json or a csv response - default is json
-			// but csv is much smaller
-			msgs := h.getter.GetStatsPort()
-			arr, err := marshalMsgs(r, msgs)
-
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-			w.WriteHeader(http.StatusOK)
-			w.Write(arr) // nolint
-			return
-		}
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}))
-	mux.Handle("/stats/username", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			// check if the request would like a json or a csv response - default is json
-			// but csv is much smaller
-			msgs := h.getter.GetStatsUsername()
-			arr, err := marshalMsgs(r, msgs)
-
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-			w.WriteHeader(http.StatusOK)
-			w.Write(arr) // nolint
-			return
-		}
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}))
-	mux.Handle("/stats/password", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			// check if the request would like a json or a csv response - default is json
-			// but csv is much smaller
-			msgs := h.getter.GetStatsPassword()
-			arr, err := marshalMsgs(r, msgs)
-
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-			w.WriteHeader(http.StatusOK)
-			w.Write(arr) // nolint
-			return
-		}
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}))
-	mux.Handle("/stats/url", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			// check if the request would like a json or a csv response - default is json
-			// but csv is much smaller
-			msgs := h.getter.GetStatsURL()
-			arr, err := marshalMsgs(r, msgs)
-
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-			w.WriteHeader(http.StatusOK)
-			w.Write(arr) // nolint
-			return
-		}
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}))
+	mux.Handle("/attacks-in-24hours", http.HandlerFunc(h.getAttacksIn24Hours))
+	mux.Handle("/stats/ip", http.HandlerFunc(h.getStatsIP))
+	mux.Handle("/stats/country", http.HandlerFunc(h.getStatsCountry))
+	mux.Handle("/stats/port", http.HandlerFunc(h.getStatsPort))
+	mux.Handle("/stats/username", http.HandlerFunc(h.getStatsUsername))
+	mux.Handle("/stats/password", http.HandlerFunc(h.getstatsPassword))
+	mux.Handle("/stats/url", http.HandlerFunc(h.getStatsURL))
 
 	go http.ListenAndServe(":"+fmt.Sprintf("%d", h.port), mux) // nolint
 	slog.Info("HTTP transport listening", "port", h.port)
@@ -201,12 +65,6 @@ func NewHTTP(config HTTPConfig) *httpTransport {
 	return &httpTransport{
 		port:   config.Port,
 		getter: config.Getter,
-	}
-}
-
-func NewServeMux() *myServeMux {
-	return &myServeMux{
-		mux: http.ServeMux{},
 	}
 }
 
@@ -230,4 +88,146 @@ func getPort(input set.Token) int {
 		return portEvent["port"].(int)
 	}
 	return 0
+}
+
+// GET funtions of API Endpoint
+func (h *httpTransport) getAttacksIn24Hours(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		msgs := h.getter.GetAttacksIn24Hours()
+		arr, err := marshalMsgs(r, msgs)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write(arr) // Check the error return value of w.Write
+		if err != nil {
+			// Handle the error appropriately
+		}
+		return
+	}
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
+func (h *httpTransport) getStatsCountry(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		msgs := h.getter.GetStatsCountry()
+		arr, err := marshalMsgs(r, msgs)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write(arr) // Check the error return value of w.Write
+		if err != nil {
+			// Handle the error appropriately
+		}
+		return
+	}
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
+func (h *httpTransport) getStatsIP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		msgs := h.getter.GetStatsIP()
+		arr, err := marshalMsgs(r, msgs)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write(arr) // Check the error return value of w.Write
+		if err != nil {
+			// Handle the error appropriately
+		}
+		return
+	}
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
+func (h *httpTransport) getStatsPort(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		msgs := h.getter.GetStatsPort()
+		arr, err := marshalMsgs(r, msgs)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write(arr) // Check the error return value of w.Write
+		if err != nil {
+			// Handle the error appropriately
+		}
+		return
+	}
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
+func (h *httpTransport) getStatsUsername(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		msgs := h.getter.GetStatsUsername()
+		arr, err := marshalMsgs(r, msgs)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write(arr) // Check the error return value of w.Write
+		if err != nil {
+			// Handle the error appropriately
+		}
+		return
+	}
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
+func (h *httpTransport) getstatsPassword(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		msgs := h.getter.GetStatsPassword()
+		arr, err := marshalMsgs(r, msgs)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write(arr) // Check the error return value of w.Write
+		if err != nil {
+			// Handle the error appropriately
+		}
+		return
+	}
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
+func (h *httpTransport) getStatsURL(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		msgs := h.getter.GetStatsURL()
+		arr, err := marshalMsgs(r, msgs)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write(arr) // Check the error return value of w.Write
+		if err != nil {
+			// Handle the error appropriately
+		}
+		return
+	}
+	w.WriteHeader(http.StatusMethodNotAllowed)
 }
